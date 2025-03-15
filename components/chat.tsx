55707452f73aaea5d055d7ec8699891e012ae2d9
Grady Corr <gcorr@centralwcu.org>
2025-03-15 12:04:35 -0700

    Fixed input box container alignment with chat messages (moved to the left)

--- components/chat.tsx
+++ components/chat.tsx
@@ -296,7 +296,7 @@ Please choose another model or start a new chat.`}
           isArtifactVisible={isArtifactVisible}
         />
 
-        <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl" onSubmit={onFormSubmit}>
+        <form className="flex mx-auto bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl" onSubmit={onFormSubmit}>
           {!isReadonly && (
             <MultimodalInput
               chatId={id}
